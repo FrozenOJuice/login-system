9ae6c320f6e75bf43aa73e847c8f51e6b4971f15
Joel deHoog <joel.dehoog@gmail.com>
2025-09-07 11:36:28 -0700

    save

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const prisma = require('./src/config/db')
+
+const app = express();
+app.use(express.json());
+
+// Test Route
+app.get('/ping', (req, res) => {
+    res.json({ message: 'pong' });
+});
+
+// DB Test Route
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch {
+        console.error(error);
+        res.status(500).json({ error: 'Database error' });
+    };
+});
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
